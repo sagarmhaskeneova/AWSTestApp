72016bd8422dc55f710842af8fc91cf235a85f52
sagar mhaske <sagarmhaske12@gmail.com>
2021-09-23 15:56:40 +0530

    nre change for catalog object

--- AWSTestApp/Controller/CatalogController.cs
+++ AWSTestApp/Controller/CatalogController.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 
 namespace AWSTestApp.Controller
@@ -22,40 +23,78 @@ namespace AWSTestApp.Controller
         {
             this.S3Client = s3client;
         }
+        //[HttpGet]
+        //public async Task<Catalog> Get(int? id)
+        //{
+        //    Catalog obj = new Catalog();
+        //    GetObjectRequest req = new GetObjectRequest();
+        //    req.BucketName = "myworkbtk";
+        //    req.Key = id + ".json";
+        //    var response = await this.S3Client.GetObjectAsync(req);
+        //    string responseBody = string.Empty;
+        //    Catalog blogObject;
+        //    using (Stream responseStream = response.ResponseStream)
+        //    using (StreamReader reader = new StreamReader(responseStream))
+        //    {
+        //        string contentType = response.Headers["Content-Type"];
+        //        responseBody = reader.ReadToEnd();
+        //        JavaScriptSerializer js = new JavaScriptSerializer();
+        //        blogObject = js.Deserialize<Catalog>(responseBody);
+        //    }
+        //    return blogObject;
+        //}
+
+
         [HttpGet]
-        public async Task<List<Catalog>> Get(int? id)
+        public async Task<List<Catalog>> Get()
         {
             Catalog obj = new Catalog();
             GetObjectRequest req = new GetObjectRequest();
             req.BucketName = "myworkbtk";
-            req.Key = id + ".json";
-            var response = await this.S3Client.GetObjectAsync(req);
+         //   var response = await this.S3Client.GetObjectAsync(req);
             string responseBody = string.Empty;
-            List<Catalog> blogObject;
-            using (Stream responseStream = response.ResponseStream)
-            using (StreamReader reader = new StreamReader(responseStream))
+            List<Catalog>  lst =new List<Catalog>();
+            //using (Stream responseStream = response.ResponseStream)
+            //using (StreamReader reader = new StreamReader(responseStream))
+            //{
+            //    string contentType = response.Headers["Content-Type"];
+            //    responseBody = reader.ReadToEnd();
+            //    JavaScriptSerializer js = new JavaScriptSerializer();
+            //    blogObject = js.Deserialize<List<Catalog>>(responseBody);
+            //}
+
+            ListObjectsV2Request request = new ListObjectsV2Request
             {
-                string contentType = response.Headers["Content-Type"];
-                responseBody = reader.ReadToEnd();
-                JavaScriptSerializer js = new JavaScriptSerializer();
-                blogObject = js.Deserialize<List<Catalog>>(responseBody);
+                BucketName = "myworkbtk",
+                MaxKeys = 10
+            };
+            ListObjectsV2Response response = await this.S3Client.ListObjectsV2Async(request);
+
+            foreach (S3Object entry in response.S3Objects)
+            {
+                Console.WriteLine("key = {0} size = {1}",
+                    entry.Key, entry.Size);
+                //blogObject.Add(new Catalog {id = entry.Key,name=entry. })
             }
-            return blogObject;
+
+            return lst;
         }
+
         [HttpPost]
-        public async Task<bool> FileUploaad(int uploadFileName)
+        public async Task<bool> FileUploaad([FromBody] Catalog obj)
         {
             PutObjectResponse response = null;
             try
             {
-                //var path = Path.Combine("Files", uploadFileName.ToString() + ".json");
-                var path = @"C:\Users\sagar\source\repos\File\" + uploadFileName + ".json";
-                using (FileStream fsSource = new FileStream(path, FileMode.Open, FileAccess.Read))
+                var path = @"C:\Users\sagar\source\repos\File\" + obj.id + ".json";
+                string json = new JavaScriptSerializer().Serialize(obj);
+                //write string to file
+                System.IO.File.WriteAllText(path, json);
+                using (FileStream fsSource = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
                 {
                     string fileExtension = Path.GetExtension(path);
                     string fileName = string.Empty;
-                    fileName = $"{DateTime.Now.Ticks}{fileExtension}";
-
+                    fileName = $"{obj.id}{fileExtension}";
                     PutObjectRequest request = new PutObjectRequest()
                     {
                         InputStream = fsSource,
@@ -63,7 +102,6 @@ namespace AWSTestApp.Controller
                         Key = fileName
                     };
                     response = await this.S3Client.PutObjectAsync(request);
-
                 }
             }
             catch (Exception ex)
